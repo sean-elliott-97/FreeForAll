9bc24a9270315cec1a2b595708f2e83955c88839
HarisonL <harisonlovinsky@gmail.com>
2021-10-24 20:33:34 -0400

    revision

---
+++ LOGSFORDOGS/script.js
@@ -0,0 +1,41 @@
+var dogData;
+var dictionaryData;
+
+//cards should be saved on refresh
+var existingDogs = JSON.parse(localStorage.getItem("dogsList"));
+if(existingDogs==null)existingDogs=[];
+else{
+$(".cardsList").append(existingDogs);
+}
+
+$("#search").click(function () {
+  dogBreed = $("#breed").val();
+
+  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+    .then((response) => response.json())
+    .then((data) => {
+      dogData = data;
+
+      dogPicture = dogData.message;
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${dogBreed}`)
+        .then((response) => response.json())
+        .then((data) => {
+          dictionaryData = data;
+          var x = "<div class = 'card'><h1>" +
+          dictionaryData[0].word +
+          "</h1>" +
+          "<img src = '" +
+          dogData.message +
+          "'>" +
+          "<p>" +
+          dictionaryData[0].meanings[0].definitions[0].definition +
+          "</p></div>";
+          existingDogs.push(x);
+
+          $(".cardsList").append(x
+           
+          );
+          localStorage.setItem("dogsList",JSON.stringify(existingDogs));
+        });
+    });
+});
