468334d71e87dcb65d66dec03c1f06d262f6e413
HarisonL <harisonlovinsky@gmail.com>
2021-10-24 22:25:09 -0400

    EVERYTHING

--- LOGSFORDOGS/script.js
+++ LOGSFORDOGS/script.js
@@ -1,14 +1,64 @@
+var wordSearch = $("#word").val();
 var dogData;
 var dictionaryData;
+//doberman is in dog api as 'doberman' but in dictionary api as 'dobermann'; if you want to see dobermans, you must type 'doberman' in the search box
+//breeds that can be searched
+var acceptableSearch = [
+  "beagle",
+  "borzoi",
+  "bouvier",
+  "chihuahua",
+  "coonhound",
+  "corgi",
+  "dachshund",
+  "dalmatian",
+  "deerhound",
+  "doberman",
+  "elkhound",
+  "greyhound",
+  "keeshond",
+  "komondor",
+  "labradoodle",
+  "labrador",
+  "leonberg",
+  "malamute",
+  "mastiff",
+  "newfoundland",
+  "papillon",
+  "pekinese",
+  "pitbull",
+  "pomeranian",
+  "poodle",
+  "pug",
+  "redbone",
+  "rottweiler",
+  "saluki",
+  "schipperke",
+  "schnauzer",
+  "setter",
+  "sheepdog",
+  "weimaraner",
+  "whippet",
+  "wolfhound",
+];
 
 //cards should be saved on refresh
 var existingDogs = JSON.parse(localStorage.getItem("dogsList"));
-if(existingDogs==null)existingDogs=[];
-else{
-$(".cardsList").append(existingDogs);
+if (existingDogs == null) existingDogs = [];
+else {
+  
+  
+  $(".cardsList").append(existingDogs);
 }
 
 $("#search").click(function () {
+  if (!acceptableSearch.includes($("#breed").val())==true) {
+    $("#breed").val("");
+
+    $("#errorMessage").css("visibility", "visible");
+    return;
+  }
+  $("errorMessage").css("visibility","invisible");
   dogBreed = $("#breed").val();
 
   fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
@@ -21,21 +71,26 @@ $("#search").click(function () {
         .then((response) => response.json())
         .then((data) => {
           dictionaryData = data;
-          var x = "<div class = 'card'><h1>" +
-          dictionaryData[0].word +
-          "</h1>" +
-          "<img src = '" +
-          dogData.message +
-          "'>" +
-          "<p>" +
-          dictionaryData[0].meanings[0].definitions[0].definition +
-          "</p></div>";
+          var x =
+            "<div class = 'card'><h1>" +
+            dictionaryData[0].word +
+            "</h1>" +
+            "<img src = '" +
+            dogData.message +
+            "'>" +
+            "<p>" +
+            dictionaryData[0].meanings[0].definitions[0].definition +
+            "</p></div>";
           existingDogs.push(x);
 
-          $(".cardsList").append(x
-           
-          );
-          localStorage.setItem("dogsList",JSON.stringify(existingDogs));
+          $(".cardsList").append(x);
+          localStorage.setItem("dogsList", JSON.stringify(existingDogs));
         });
     });
+    $("#clear").css("visibility","visible");
 });
+$("#clear").click(function(){
+localStorage.clear();
+location.reload();
+
+})
