80ab6c95479a34a84ba5ca15dc5a42fccc0e1498
sean-elliott-97 <sean.elliott.33197@gmail.com>
2021-10-26 12:57:42 -0400

    styled availableBreeds, added readme file, coverted user input to lowercase

---
+++ README.md
@@ -0,0 +1,13 @@
+# Logs-for-Dogs
+
+This app is used to create cards containing a dog breed name, description, and random image associated with the dog breed.
+
+To know which dog breeds are searchable, users can click on the instructions tab on the navbar.
+
+After learning which breeds are searchable, users can go back to the main page and enter a breed into the search bar.
+
+After entering a searchable breed into the search bar, a card containing the breed name, description, and random image will be added. 
+
+Additionally, a clear button will be displayed, that allows users to clear the card(s).
+
+If the user leaves the webpage and comes back, the added cards will remain.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var wordSearch = $("#word").val();
 var dogData;
 var dictionaryData;
+//displays when image is loading
+var spinner =
+  "<div id ='spinner' class='preloader-wrapper small active'><div class='spinner-layer spinner-green-only'><div class='circle-clipper left'><div class='circle'></div></div><div class='gap-patch'><div class='circle'></div></div><div class='circle-clipper right'><div class='circle'></div></div></div></div>";
 
 //breeds that can be searched
 var acceptableSearch = [
@@ -52,45 +55,48 @@ else {
 
 //allows users to search for random dog images by breed; creates a card with random dog image, breed and definition
 $("#search").click(function () {
-  if (!acceptableSearch.includes($("#breed").val()) == true) {
+  if (!acceptableSearch.includes($("#breed").val().toLowerCase()) == true) {
     $("#breed").val("");
 
     $("#errorMessage").css("visibility", "visible");
     return;
   } else {
     $("#errorMessage").css("visibility", "hidden");
-    dogBreed = $("#breed").val();
+    //converts dogBreed to lowercase
+    dogBreed = $("#breed").val().toLowerCase();
   }
-  $(".cardsList").append('');
+  //adds loading spinner while card is being created
+  $(".cardsList").append(spinner);
   fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
     .then((response) => response.json())
     .then((data) => {
       dogData = data;
 
       dogPicture = dogData.message;
-      
+
       fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${dogBreed}`)
         .then((response) => response.json())
         .then((data) => {
           dictionaryData = data;
           var x =
-            "<div class = 'card'><h1>" +
+            "<div class = 'card'><h1 class = 'dogBreedName'>" +
             dictionaryData[0].word +
             "</h1>" +
             "<img src = '" +
             dogData.message +
             "'>" +
-            "<p>" +
+            "<p class = 'dogDefinition'>" +
             dictionaryData[0].meanings[0].definitions[0].definition +
             "</p></div>";
           existingDogs.push(x);
 
           $(".cardsList").append(x);
           localStorage.setItem("dogsList", JSON.stringify(existingDogs));
-          
+          //clears input field
+          $("#breed").val("");
         });
-       
-     
+      //removes the spinner after card is displayed
+      $("#spinner").remove();
     });
   $("#clear").css("visibility", "visible");
 });
